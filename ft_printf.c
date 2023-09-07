11330e4a6b796447e43f7bcced27c7f60fddcdc6
Jonas Vets <jvets@student.42sp.org.br>
2023-09-07 19:37:29 -0300

    added %% functionality

--- ft_printf.c
+++ ft_printf.c
@@ -6,13 +6,13 @@
 /*   By: jvets <jvets@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/06 20:53:07 by jvets             #+#    #+#             */
-/*   Updated: 2023/09/07 15:57:44 by jvets            ###   ########.fr       */
+/*   Updated: 2023/09/07 19:31:09 by jvets            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_printf.h"
 
-void	print_format(const char *str, va_list ap);
+void	print_format(const char **str, va_list ap);
 
 int	ft_printf(const char *str, ...)
 {
@@ -23,21 +23,26 @@ int	ft_printf(const char *str, ...)
 	c = 0;
 	while (*str)
 	{
-		if (*str == '%')
-			print_format(++str, ap);
+		if (*str == '%' && str[1] != '%')
+			print_format(&str, ap);
+		//else if (*str == '%' && str[1] == '%')
+		//	str += 2;
 		else
 			c += write(1, str, 1);
+		if (*str == '%' && str[1] == '%')
+			str++;
 		str++;
 	}
 	va_end(ap);
 	return (c);
 }
 
-void	print_format(const char *str, va_list ap)
+void	print_format(const char **str, va_list ap)
 {
 	int	character;
 
-	if (*str == 'c')
+	(*str)++;
+	if (**str == 'c')
 	{
 		character = va_arg(ap, int);
 		write(1, &character, 1);
--- ft_printf.h
+++ ft_printf.h
@@ -6,16 +6,16 @@
 /*   By: jvets <jvets@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/06 21:16:31 by jvets             #+#    #+#             */
-/*   Updated: 2023/09/07 15:27:28 by jvets            ###   ########.fr       */
+/*   Updated: 2023/09/07 19:24:35 by jvets            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef FT_PRINTF_H
 # define FT_PRINTF_H
 
-#include <stdarg.h>
-#include <unistd.h>
+# include <stdarg.h>
+# include <unistd.h>
 
-int     ft_printf(const char *str, ...);
+int	ft_printf(const char *str, ...);
 
 #endif
